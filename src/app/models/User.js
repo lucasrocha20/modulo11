16f69f2cbab6d352299ec7fd054945b51b8c7a60
Lucas Roch <lucas_rocha.14@outlook.com>
2019-08-28 22:10:07 -0300

    chore: teste de criacao de usuario

---
+++ src/app/models/User.js
@@ -0,0 +1,19 @@
+import Sequeliza, { Model, Sequelize } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+}
+
+export default User;
